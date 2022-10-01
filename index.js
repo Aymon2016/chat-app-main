20be8ad40079cc4242b87b4174daa501f07dfec8
Aymon2016 <mdaiman2016@gmail.com>
2022-10-01 12:20:09 +0600

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect(process.env.MONGO_CONNETION_STRING,{
+mongoose.connect('mongodb+srv://AYMON:<AYMON>@cluster0.gq9xpkk.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedtopology:true,
 })
@@ -31,8 +31,7 @@ if (process.env.NODE_ENV ==='production'){
   })
 }
 
-
-const PORT=process.env.PORT || 5000;
+const PORT=8000;
 
 const server = app.listen(PORT,()=>{
     console.log(`app listening to port ${PORT}`)
