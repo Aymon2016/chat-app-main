87f3fbdba5de9ecb4be5f95c2b18390bd07fd676
Aymon2016 <mdaiman2016@gmail.com>
2022-10-09 01:24:59 +0600

    hello

--- client/src/utils/apiRoute.js
+++ client/src/utils/apiRoute.js
@@ -1,4 +1,5 @@
-export const host = `http://localhost:${process.env.PORT}`;
+export const host = "http://localhost:8000";
+
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
